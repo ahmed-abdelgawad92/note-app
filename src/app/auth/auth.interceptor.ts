38e28d37093acc50809af497ead9f4a0772a2c67
Ahmed Abdelgawad <ahmed.elkayaty92@gmail.com>
2018-12-14 00:05:52 +0100

    bbbb

--- src/app/auth/auth.interceptor.ts
+++ src/app/auth/auth.interceptor.ts
@@ -1,8 +1,8 @@
 import { HttpClient, HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { AuthService } from "./auth.service";
-import { Injectable } from "@angular/core"
-import { tap } from "rxjs/Operators";
+import { Injectable } from "@angular/core";
+import { tap } from "rxjs/operators";
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor{
     constructor(private auth: AuthService, private http: HttpClient){}
